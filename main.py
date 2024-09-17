4067b1fc9e79b8bbe20ab1330e051b115b2303e1
Jon Klein <jonklein2021@gmail.com>
2024-09-16 20:12:20 -0400

    basic TCP server setup

---
+++ main.py
@@ -0,0 +1,17 @@
+from socket import *
+
+# set up TCP server with parameters
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('',serverPort))
+serverSocket.listen(1)
+
+print('Server listening on port', serverPort)
+
+while True:
+    connectionSocket, addr = serverSocket.accept()
+
+    sentence = connectionSocket.recv(1024).decode()
+    capitalizedSentence = sentence.upper()
+    connectionSocket.send(capitalizedSentence.encode())
+    connectionSocket.close()
