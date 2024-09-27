dd8982b9e91fca5158cc9c22e6bab95960be43de
Jon Klein <jonklein2021@gmail.com>
2024-09-26 22:56:06 -0400

    final touches

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# python-web-server
+# Python Web Server
+
 Basic HTTP server written in Python serving basic requests for my Fundamentals of Internetworking class at Lehigh
--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from socket import *
 import urllib.request
 
 # get public IP
-publicip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+# source: https://stackoverflow.com/questions/2311510/getting-a-machines-external-ip-address-with-python
+publicIP = urllib.request.urlopen('https://ident.me').read().decode('utf8')
 
 # set up TCP server with parameters
 serverPort = 8080
@@ -14,10 +15,10 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 
-# store logs here
-logs = []
+# write logs here
+logFile = "log.txt"
 
-print(f'Server listening on 127.0.0.1:{serverPort} and {publicip}:{serverPort}')
+print(f'Server listening on 127.0.0.1:{serverPort} and {publicIP}:{serverPort}')
 
 # loop to keep server running
 while True:
@@ -25,43 +26,51 @@ while True:
     connectionSocket, addr = serverSocket.accept()
     ip, port = addr
     connectionSocket.settimeout(10)
-    print(f'Connection from {ip}:{port}')
+    
+    # log new connection
+    log = f'Connection from {ip}:{port}'
+    print(log)
+    with open(logFile, "a") as f:
+        f.write(log + "\n")
     
     # receive message from client or timeout
     try:
+        # need to ignore errors to handle non-ASCII characters in images
         sentence = connectionSocket.recv(1024).decode(errors='ignore')
+        
+        # source: https://stackoverflow.com/questions/7999935/python-datetime-to-string-without-microsecond-component
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         request = sentence.split('\n')[0].split(' ')
         
         # verify the request is a HTTP GET request
         if request[0] != "GET":
             print("Invalid GET request. Closing connection.")
-            connectionSocket.close()
-            break # TODO: change this
+            response = "HTTP/1.1 501 Not Implemented\n\n501 Not Implemented"
+            connectionSocket.send(response.encode())
+            continue
         
         # determine the requested file; recall that "/" => "index.html"
         requestedFile = "static" + (request[1] if request[1] != "/" else "/index.html")
         
         # append request to logs
-        logs.append(f"[{timestamp}] {ip}:{port} requests {requestedFile}\n")
+        log = f"[{timestamp}] {ip}:{port} requests {requestedFile}"
+        print(log)
+        with open(logFile, "a") as f:
+            f.write(log + "\n")
         
         # check if file exists
         try:
             with open(requestedFile, 'rb') as file:
-                # send response to client
+                # build response and send to client
                 response = b"HTTP/1.1 200 OK\n\n" + file.read()
                 connectionSocket.send(response)
         except FileNotFoundError:
-            # send response to client
+            # send 404 to client
             response = "HTTP/1.1 404 Not Found\n\n404 Not Found"
             connectionSocket.send(response.encode())
         
     except timeout:
         print("Client timed out.")
     finally:
-        # write logs to log.txt and close connection
-        with open("log.txt", "a") as logfile:
-            logfile.writelines(logs)
-            
-        connectionSocket.close()
-    
+        # close connection
+        connectionSocket.close()    
